0bd73bae1f4333fedeb6dbc6b4633f247f7e36d4
yuneco <yukiyuneco@gmail.com>
2021-01-31 00:45:32 +0900

    Create a blank vue project with pixi

---
+++ src/components/GameStage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="GameStageRoot">
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import * as PIXI from 'pixi.js'
+
+const initPixiStage = (canvas: HTMLCanvasElement) => {
+  const pixiApp = new PIXI.Application({
+    width: 600,
+    height: 600,
+    view: canvas,
+    backgroundColor: 0xcccccc,
+    resolution: window.devicePixelRatio || 1,
+    antialias: true,
+    autoDensity: true,
+    transparent: true
+  })
+}
+
+export default defineComponent({
+  name: 'GameStage',
+  setup() {
+    const canvas = ref<HTMLCanvasElement>()
+    onMounted(() => {
+      if (canvas.value) {
+        initPixiStage(canvas.value)
+      }
+    })
+    return {
+      canvas
+    }
+  }
+})
+</script>
